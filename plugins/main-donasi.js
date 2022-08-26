06fa94dc17e847b79a5d24d08c3f58c38773dd57
Farhamdhani <112062661+Farhamdhani@users.noreply.github.com>
2022-08-26 18:41:50 +0800

    Update main-donasi.js

--- plugins/main-donasi.js
+++ plugins/main-donasi.js
@@ -2,13 +2,13 @@ const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessa
 let fs = require('fs')
 let handler = async (m) => {
 let krizyn = `
-Hallo User DaniBotz 👋
+Hallo User FarhanBotz 👋
 *Kalian Bisa Mendukung Saya Agar Bot Ini Tetap Up To Date Dengan Cra:*
 
 ╭─❏ *『 DONASI 』*
-│ ➪ Dana : 081251549899
-│ ➪ Gopay : 081352848936
-│ ➪ Ovo : 081352848946
+│ ➪ Dana : 089509381243
+│ ➪ Gopay : "_"
+│ ➪ Ovo : "_"
 ╰───────────────❏
 
 BERAPAPUN DONASI KALIAN AKAN SANGAT BERARTI 👍
@@ -30,7 +30,7 @@ TERIMAKASIH  BUAT YANG SUDAH MAU BERDONASI😉
            },
                 {
                urlButton: {
-               displayText: '😼Owner DaniBotz',
+               displayText: '😼Owner FarhanBotz',
                url: nomer
              }
 
